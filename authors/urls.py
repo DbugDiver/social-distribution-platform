588b1403b3456ec63e04e489cbe8e2be8bb0ff3e
DbugDiver <birparkash00@gmail.com>
2026-02-24 22:37:39 -0700

    Remove backend modifled authors/urls , socialdistribution/urls update requirements

--- authors/urls.py
+++ authors/urls.py
@@ -2,8 +2,16 @@ from django.urls import path
 
 from . import views
 
+'''
 urlpatterns = [
     path("", views.home_feed, name="home"),
     path("authors/<uuid:pk>/", views.author_profile, name="author-profile"),
+    path("profile/edit/", views.edit_profile, name="edit-profile"),
+]
+'''
+# changes made to merge backend and social distribution
+urlpatterns = [
+    path("", views.home_feed, name="home-feed"),
+    path("<uuid:pk>/", views.author_profile, name="author-profile"),
     path("profile/edit/", views.edit_profile, name="edit-profile"),
 ]
--- socialdistribution/urls.py
+++ socialdistribution/urls.py
@@ -3,12 +3,24 @@ from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 
+'''
 urlpatterns = [
     # Include the authentication URLs provided by Django
     path("accounts/", include("django.contrib.auth.urls")),
     path("", include("posts.urls")), 
     path("admin/", admin.site.urls),
+    path("", include("authors.urls"))
 ]
+'''
+# Changes made for merging backened and social distribution
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("accounts/", include("django.contrib.auth.urls")),
+
+    path("", include("posts.urls")),           # homepage = posts stream
+    path("authors/", include("authors.urls")),
+]
+
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
