c223466d0536485ca904494e98a9be4df21e8f97
Rosy Budhathoki <budhatho@ualberta.ca>
2026-03-12 13:57:22 -0600

    Fixing css not loading issues

--- socialdistribution/settings.py
+++ socialdistribution/settings.py
@@ -8,22 +8,13 @@ import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+DEBUG = os.environ.get("DEBUG", "True") == "True"  # defaults to True locally
 SECRET_KEY = os.environ.get(
     "SECRET_KEY",
     "django-insecure-vsc=qpkdlfthsm)na5b4hf9q!tiff#!cg00@=*mn@#h!+cd_))"
 )
 
-DEBUG = os.environ.get("DEBUG", "False") == "True"
-
-ALLOWED_HOSTS = [
-    "127.0.0.1",
-    "localhost",
-    ".herokuapp.com",
-]
-
-CSRF_TRUSTED_ORIGINS = [
-    "https://*.herokuapp.com",
-]
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "*").split(",")
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
@@ -81,7 +72,7 @@ DATABASES = {
     "default": dj_database_url.config(
         default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
         conn_max_age=600,
-        ssl_require=False,
+        conn_health_checks=True,
     )
 }
 
@@ -112,8 +103,13 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
+CLOUDINARY_STORAGE = {
+    "CLOUD_NAME": os.environ.get("CLOUDINARY_CLOUD_NAME"),
+    "API_KEY": os.environ.get("CLOUDINARY_API_KEY"),
+    "API_SECRET": os.environ.get("CLOUDINARY_API_SECRET"),
+}
+
+DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 AUTH_USER_MODEL = "authors.Author"
-
-DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
