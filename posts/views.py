648c90234bb252ce79b5191e2581063e6cd0462d
Rosy Budhathoki <budhatho@ualberta.ca>
2026-03-17 19:54:08 -0600

    Fix indent

--- posts/views.py
+++ posts/views.py
@@ -119,49 +119,49 @@ def stream(request):
     # --- REMOTE POSTS ---
     class RemotePost:
     """Wrap remote post JSON to behave like a Post object for the template"""
-    def __init__(self, data, node_url):
-        # Copy data
-        self.id = data.get("id")
-        self.title = data.get("title")
-        self.content = data.get("content")
-        self.content_type = data.get("content_type")
-        self.visibility = data.get("visibility")
-        self.image = data.get("image")  # string URL from remote
-        self.rendered = md.markdown(self.content or "", extensions=["extra"]) if self.content_type == "text/markdown" else self.content
-        self.like_count = data.get("like_count", 0)
-        self.comment_count = len(data.get("comments", []))
-        self.liked_by_me = False
-        self.remote = True
-        self.node_url = node_url
+        def __init__(self, data, node_url):
+            # Copy data
+            self.id = data.get("id")
+            self.title = data.get("title")
+            self.content = data.get("content")
+            self.content_type = data.get("content_type")
+            self.visibility = data.get("visibility")
+            self.image = data.get("image")  # string URL from remote
+            self.rendered = md.markdown(self.content or "", extensions=["extra"]) if self.content_type == "text/markdown" else self.content
+            self.like_count = data.get("like_count", 0)
+            self.comment_count = len(data.get("comments", []))
+            self.liked_by_me = False
+            self.remote = True
+            self.node_url = node_url
 
-        # Parse created string into datetime
-        created_str = data.get("created")
-        try:
-            self.created = datetime.fromisoformat(created_str) if created_str else datetime.min
-        except ValueError:
-            self.created = datetime.min
+            # Parse created string into datetime
+            created_str = data.get("created")
+            try:
+                self.created = datetime.fromisoformat(created_str) if created_str else datetime.min
+            except ValueError:
+                self.created = datetime.min
 
-        # Wrap author dict into an object that template can use
-        author_data = data.get("author", {})
-        self.author = type("AuthorObj", (), {
-            "username": author_data.get("username", "Unknown"),
-            "profileImage": author_data.get("profileImage", None)  # string URL or None
-        })
-
-        # Wrap comments into objects that template can use
-        self.comment_list = []
-        for c in data.get("comments", [])[:3]:
-            comment_author = type("AuthorObj", (), {
-                "username": c.get("author", {}).get("username", "Unknown")
+            # Wrap author dict into an object that template can use
+            author_data = data.get("author", {})
+            self.author = type("AuthorObj", (), {
+                "username": author_data.get("username", "Unknown"),
+                "profileImage": author_data.get("profileImage", None)  # string URL or None
             })
-            comment_obj = type("CommentObj", (), {
-                "id": c.get("id"),
-                "comment": c.get("comment"),
-                "author": comment_author,
-                "like_count": c.get("like_count", 0),
-                "liked_by_me": False
-            })
-            self.comment_list.append(comment_obj)
+
+            # Wrap comments into objects that template can use
+            self.comment_list = []
+            for c in data.get("comments", [])[:3]:
+                comment_author = type("AuthorObj", (), {
+                    "username": c.get("author", {}).get("username", "Unknown")
+                })
+                comment_obj = type("CommentObj", (), {
+                    "id": c.get("id"),
+                    "comment": c.get("comment"),
+                    "author": comment_author,
+                    "like_count": c.get("like_count", 0),
+                    "liked_by_me": False
+                })
+                self.comment_list.append(comment_obj)
 
     current_node = request.build_absolute_uri("/").rstrip("/")
     remote_posts = []
