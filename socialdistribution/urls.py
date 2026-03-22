6ecddcfa3f73dd827da006e4a673b48ae18eb84b
BetterThanYou73 <manas2005mj@gmail.com>
2026-03-21 19:22:45 -0600

    Serve uploaded media files in production

--- socialdistribution/urls.py
+++ socialdistribution/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
+from django.urls import re_path
 
 '''
 urlpatterns = [
@@ -24,3 +26,8 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# Serve user-uploaded media in production deployments without external media storage.
+urlpatterns += [
+    re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
+]
