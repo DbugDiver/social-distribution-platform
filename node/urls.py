72a551c5f10656c915a2669514b0c0fef9e5dc0b
Gui Carius <carius@ualberta>
2026-04-01 20:22:54 -0600

    feature: disable/enable branch

--- node/urls.py
+++ node/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path("dashboard/", views.node_admin_dashboard, name="node-admin-dashboard"),
     path("management/", views.manage_nodes, name="manage-nodes"),
     path("management/<int:node_id>/delete/", views.delete_node, name="delete-node"),
+    path("manage-nodes/<int:node_id>/toggle/", views.toggle_node, name="toggle-node"),
     path("approvals/", views.approvals, name="approvals"),
     path("authors/", views.manage_authors, name="manage-authors"),
     path("add-author/", views.add_author_page, name="add-author"),
